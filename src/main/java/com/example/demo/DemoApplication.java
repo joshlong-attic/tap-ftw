1fa53078ed858b4c2f960e8fb90264ec193c2bb7
Josh Long <josh@joshlong.com>
2024-02-26 13:29:51 -0800

    YOLO

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,25 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.util.Map;
+
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+import static org.springframework.web.servlet.function.ServerResponse.ok;
+
 
 @SpringBootApplication
 public class DemoApplication {
+
+	@Bean
+	RouterFunction<ServerResponse> tap() {
+		return route()
+				.GET("/hello", request -> ok().body(Map.of("message", "Hodor TAP!")))
+				.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
